e6259258ba9b84483adc4efe7e097530432c9cb6
jzonthemtn <jeffrey@mtnfog.com>
2017-08-25 07:32:42 -0400

    Updating script.

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # This script gets the AWS Instance Metadata Simulator.
 
 sudo yum -y install git golang
-mkdir ~/go
+mkdir -p ~/go
 export GOPATH=~/go
 go get -u github.com/mtnfog/aws-metadata-emulator
 cd ~/go/bin
 wget https://raw.githubusercontent.com/mtnfog/aws-metadata-simulator/master/metadata.toml
 sudo iptables -t nat -A OUTPUT -p tcp -d 169.254.169.254 --dport 80 -j DNAT --to-destination 127.0.0.1:8080
-echo "Edit the metadata.toml to have the values you need and then run ./aws-metadata-simulator"
+echo "Edit the metadata.toml to have the values you need and then run ~/go/bin/aws-metadata-emulator"
