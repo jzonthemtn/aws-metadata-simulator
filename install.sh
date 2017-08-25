a8f542d4d38cc27a0ccc4635380fe5ce289c8e28
jzonthemtn <jeffrey@mtnfog.com>
2017-08-25 07:40:59 -0400

    Updating script.

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ mkdir -p ~/go
 export GOPATH=~/go
 go get -u github.com/mtnfog/aws-metadata-emulator
 cd ~/go/bin
-wget https://raw.githubusercontent.com/mtnfog/aws-metadata-simulator/master/metadata.toml
+wget https://raw.githubusercontent.com/mtnfog/aws-metadata-simulator/master/metadata.toml -O ~/go/bin/metadata.toml
 sudo iptables -t nat -A OUTPUT -p tcp -d 169.254.169.254 --dport 80 -j DNAT --to-destination 127.0.0.1:8080
 echo "Edit the metadata.toml to have the values you need and then run ~/go/bin/aws-metadata-emulator"
